5ac9ff972328fe944099b1a2ae2bf3ce4889c473
Sivagami34 <ksivagami2u@gmail.com>
2024-12-17 16:59:41 +0000

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -34,3 +34,39 @@ plt.show()
 #categorical data with bar plot
 plt.bar(["male", "female"], [12, 15])
 plt.show()
+
+#histogram
+n = np.random.randint(1, 60, 20)
+print(n)
+r = np.arange(0, 61, 10)
+plt.hist(n, r,rwidth = 0.8)
+plt.show()
+
+#scatter
+plt.scatter(x, y, color = "red", marker = "*", s = 40)
+plt.show()
+
+#pie chaet
+a = ["sleeping", "eating", "studying", "reading", "watching TV"]
+v = [8, 2, 5, 3, 2]
+c = ["red", "blue", "pink", "green", "purple"]
+plt.pie(v, labels = a, colors = c, autopct='%1.1f%%', startangle = 90, shadow = True)
+plt.show()
+
+#stack plot
+d = ["monday", "tuesday", "wednesday", "thurday", "friday"]
+sleeping = [8, 7, 9, 6, 8]
+eating = [1, 2, 1, 2, 3]
+studying = [4, 5, 6, 5, 6]
+reading = [2, 4, 3, 5, 4]
+plt.stackplot(d, sleeping, eating, studying, reading, labels = ["sleeping", "eating", "studying", "reading"])
+plt.legend()
+plt.show()
+
+#sub plots
+plt.subplot(2, 2, 2)
+plt.stackplot(d, sleeping, eating, studying, reading, labels = ["sleeping", "eating", "studying", "reading"])
+plt.subplot(2, 2, 3)
+plt.pie(v, labels = a, colors = c, autopct='%1.1f%%', startangle = 90, shadow = True)
+plt.legend()
+plt.show()
