1d4b6b92bc41761e8c8821f58790382313727415
Sivagami34 <ksivagami2u@gmail.com>
2025-01-05 09:07:52 +0000

    Update irisplots.py

--- irisplots.py
+++ irisplots.py
@@ -7,3 +7,15 @@ p = df.groupby("species").count()
 print(p)
 plt.pie(p["sepal_length"], labels = p.index)
 plt.show()
+
+#Create subplots with 4 scatter plots between: petal length and petal width, 
+#petal length and sepal width, sepal length and sepal width, petal wodth and sepal width
+plt.subplot(2, 2, 1)
+plt.scatter(df["petal_length"], df["petal_width"])
+plt.subplot(2, 2, 2)
+plt.scatter(df["petal_length"], df["sepal_width"])
+plt.subplot(2, 2, 3)
+plt.scatter(df["sepal_length"], df["sepal_width"])
+plt.subplot(2, 2, 4)
+plt.scatter(df["petal_width"], df["sepal_width"])
+plt.show()
