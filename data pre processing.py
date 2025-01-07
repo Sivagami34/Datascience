6ea11559306bab05d40fe23cca999c5dacce2880
Sivagami34 <ksivagami2u@gmail.com>
2025-01-07 16:58:44 +0000

    Update data pre processing.py

--- data pre processing.py
+++ data pre processing.py
@@ -7,5 +7,31 @@ print(df)
 print(df.isnull().sum())
 
 #remove missing values
-df.dropna(inplace = True)
+#df.dropna(inplace = True)
+#print(df)
+
+#treating missing values
+#manual
+
+'''a = df["Age"].mean()
+df.loc[df["Age"].isnull(),"Age"] = a
 print(df)
+'''
+
+#imputing
+from sklearn.impute import SimpleImputer
+s = SimpleImputer(missing_values = pd.NA, strategy = "mean")
+df[["Age", "Salary"]] = s.fit_transform(df[["Age", "Salary"]])
+print(df)
+
+#separate features
+X = df[["Country","Age", "Salary"]]
+y = df["Purchased"]
+
+#encode categorical features
+X = pd.get_dummies(X, columns = ["Country"], dtype = int)
+print(X)
+#encoding target value
+#manual
+y.replace({"No": 0, "Yes": 1}, inplace = True)
+print(y)
