e4d6d7f8fcfe00275fcb52d4d2e3a630dd6ed590
Sivagami34 <ksivagami2u@gmail.com>
2025-01-12 09:51:48 +0000

    Update data pre processing.py

--- data pre processing.py
+++ data pre processing.py
@@ -24,7 +24,7 @@ s = SimpleImputer(missing_values = pd.NA, strategy = "mean")
 df[["Age", "Salary"]] = s.fit_transform(df[["Age", "Salary"]])
 print(df)
 
-#separate features
+#separate features and target
 X = df[["Country","Age", "Salary"]]
 y = df["Purchased"]
 
@@ -33,5 +33,25 @@ X = pd.get_dummies(X, columns = ["Country"], dtype = int)
 print(X)
 #encoding target value
 #manual
-y.replace({"No": 0, "Yes": 1}, inplace = True)
+#y.replace({"No": 0, "Yes": 1}, inplace = True)
+#print(y)
+
+#label encode
+from sklearn.preprocessing import LabelEncoder
+l = LabelEncoder()
+y = l.fit_transform(y)
 print(y)
+
+#standard scaling
+from sklearn.preprocessing import StandardScaler
+sd = StandardScaler()
+X["Salary"] = sd.fit_transform(X[["Salary"]])
+print(X)
+
+#split data into training set and test set
+from sklearn.model_selection import train_test_split
+Xtrain, Xtest, ytrain, ytest= train_test_split(X, y, train_size = 0.8, random_state = 60)
+print("Xtrain\n", Xtrain)
+print("Xtest\n", Xtest)
+print("ytrain\n", ytrain)
+print("ytest\n", ytest)
