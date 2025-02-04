c34832a05a6bdf90c1742a0478413ff913b04387
Sivagami34 <ksivagami2u@gmail.com>
2025-02-04 16:57:22 +0000

    Update who covid19.py

--- who covid19.py
+++ who covid19.py
@@ -9,3 +9,17 @@ df = df[["DateReported", "Country", "New_cases", "Cumulative_cases", "New_deaths
 d = df.groupby("DateReported")
 sum = d.sum()
 print(sum)
+print(sum.columns)
+import matplotlib.pyplot as plt
+plt.plot(sum.index, sum["Cumulative_cases"])
+plt.title("daily total cases worldwide")
+plt.show()
+#daily new cases worldwide
+plt.plot(sum.index, sum["New_cases"])
+plt.show()
+
+country = input("Enter country: ")
+c = df.loc[df["Country"] == country]
+print(c)
+plt.plot(c["DateReported"], c["New_cases"])
+plt.show()
