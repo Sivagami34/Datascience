662131ee9e9c3ac46e032f872ab7c6f25c31bb96
Sivagami34 <ksivagami2u@gmail.com>
2025-02-11 16:58:12 +0000

    Update supermarket sales.py

--- supermarket sales.py
+++ supermarket sales.py
@@ -12,3 +12,15 @@ nc = df["Customer type"].value_counts()
 print(nc)
 plt.bar(nc.index, nc.values)
 plt.show()
+#Find the number of male and female members. Show them on a pie chart.
+m = df[df["Customer type"] == "Member"]
+gc = m["Gender"].value_counts()
+print(gc)
+plt.pie(gc, labels = gc.index, autopct='%1.1f%%')
+plt.show()
+#Create a line plot with day-wise total sales.
+g = df.groupby("Date")
+s = g["Total"].sum()
+print(s)
+plt.plot(s.index, s)
+plt.show()
